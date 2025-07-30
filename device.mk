5c32e55df93d64cc217659caa1299301ddcd7504
kyasu <kyasu.dev@gmail.com>
2025-07-30 23:06:15 +0900

    pipa: Disable `thumbnail_block_model` of libstagefright
    * Yes we are affected too....
    
    Change-Id: I86f92335af282d3de508150bbbcb3026993ec48c

--- device.mk
+++ device.mk
@@ -247,6 +247,8 @@ PRODUCT_PACKAGES += \
     media_codecs_performance_kona.xml \
     video_system_specs.json
 
+$(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,true)
+
 # OMX
 PRODUCT_PACKAGES += \
     libOmxCore \
