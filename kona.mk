f4de1e188c0effe56455cebb8782efb1d86cd187
CuriousNom <aryanbiswas7585@gmail.com>
2026-02-10 14:48:36 +0530

    pipa: Move task profiles to power section
    Change-Id: I65f924c98709c187113827e7aac7c8034e21b3ba

--- kona.mk
+++ kona.mk
@@ -155,11 +155,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
-# Cgroup and task_profiles
-PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
@@ -357,6 +352,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Public libraries
 PRODUCT_COPY_FILES += \
